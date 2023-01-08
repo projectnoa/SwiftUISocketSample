f151783a7047085b2316158aa048d639089aa431
Juan <projectnoa@users.noreply.github.com>
2023-01-08 16:20:17 -0700

    Initial Commit

---
+++ SwiftUISocketSample/SwiftUISocketSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//	SwiftUISocketSampleApp.swift
+//  SwiftUISocketSample
+//
+//  Created by Juan Mueller on 1/8/23.
+//  For more, visit www.ajourneyforwisdom.com
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISocketSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUISocketSampleUITests/SwiftUISocketSampleUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//	SwiftUISocketSampleUITestsLaunchTests.swift
+//  SwiftUISocketSampleUITests
+//
+//  Created by Juan Mueller on 1/8/23.
+//  For more, visit www.ajourneyforwisdom.com
+//
+
+import XCTest
+
+final class SwiftUISocketSampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
